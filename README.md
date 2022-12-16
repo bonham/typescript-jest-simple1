8b7747f99a72b6bb9fb92239face5e9f3c9f7b69
Art Furrer <2823246+bonham@users.noreply.github.com>
2022-12-17 00:14:09 +0100

    works

---
+++ README.md
@@ -0,0 +1,3 @@
+Used this, but with npm instead of yarn
+
+https://medium.com/swlh/jest-with-typescript-446ea996cc68
---
+++ dummy.test.ts
@@ -0,0 +1,5 @@
+import { sum } from "./dummy"
+test("Dummy unit test", () => {
+  const actual = sum(1,2)
+  expect(actual).toBe(3);
+});
